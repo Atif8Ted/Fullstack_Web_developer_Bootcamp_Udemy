bf66311c658f0475b3c4181597c5e8b6c1d447ec
Atif8Ted <mohd.atifimam@hotmail.com>
2017-09-09 12:51:17 +0530

    added

---
+++ javascript/l1.js
@@ -0,0 +1,22 @@
+undefined
+//alert
+undefined
+alert("Atif Imam")
+undefined
+alert(10+2);
+undefined
+cons
+VM1113:1 Uncaught ReferenceError: cons is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM1113:1
+console.log("Hello from the other side");
+entry-vendor.ecad7ef9007b76ff3506.js:238 Hello from the other side
+undefined
+//prompt
+undefined
+prompt("What is your name");
+"Atif"
+var name=prompt("What is your name");
+undefined
+name
+"Atif"
