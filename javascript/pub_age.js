f090be9a09fb40dac487a7b855fee6104d777e6f
Atif8Ted <mohd.atifimam@hotmail.com>
2017-09-09 16:22:11 +0530

    added

---
+++ javascript/pub_age.js
@@ -0,0 +1,14 @@
+var age;
+
+age=prompt("Enter your age");
+if(age<18){
+console.log("Sorry ,you are underage");
+}
+
+else if(age>18 && age<21){
+console.log("You cant drink");
+}
+
+else{
+console.log("Do whatever you want to do ");
+}
