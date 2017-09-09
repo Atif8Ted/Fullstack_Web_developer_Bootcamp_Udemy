7530fbe2d1c8c82645620321d1d78d38884f5593
Atif8Ted <mohd.atifimam@hotmail.com>
2017-09-09 16:04:43 +0530

    added

---
+++ javascript/boolean.js
@@ -0,0 +1,34 @@
+var x=99;
+undefined
+x=="99";
+true
+x==="99";
+false
+//
+undefined
+var y=null;
+undefined
+y==undefined;
+true
+y===undefined;
+false
+true=='1'
+true
+true==1
+true
+true===1
+false
+false==0
+true
+Nan==Nan
+VM1519:1 Uncaught ReferenceError: Nan is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM1519:1
+NaN=NaN
+NaN
+NaN==NaN
+false
+null=undefined
+VM1568:1 Uncaught ReferenceError: Invalid left-hand side in assignment
+null==undefined
+true
