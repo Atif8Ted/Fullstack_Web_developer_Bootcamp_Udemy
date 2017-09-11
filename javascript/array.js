319392335b43eeff3804fcf10488449243f79adf
Atif8Ted <mohd.atifimam@hotmail.com>
2017-09-12 01:29:45 +0530

    added

---
+++ javascript/array.js
@@ -0,0 +1,11 @@
+var friends=["Charlie","Liz","Peter"];
+var friends=["Charlie","Liz","Peter"];
+
+
+console.log(friends);
+(3) ["Charlie", "Liz", "Peter"]
+
+console.log(friends[1]);
+VM2372:1 Liz
+friends[1] +"<3"+ friends[2];
+"Liz<3Peter"
