aa7d1cee184db015686b5a797fe9e5eabce0b31f
Atif8Ted <mohd.atifimam@hotmail.com>
2017-09-12 01:56:44 +0530

    added

--- javascript/array_method.js
+++ javascript/array_method.js
@@ -13,3 +13,29 @@ var friends=["Charlie","Liz","Peter","Liz"];
 friends.indexOf("Liz");
 friends.indexOf("Liz");
 friends.indexOf("Swati");
+
+//slice
+var friends=["Charlie","Liz","Peter","Liz"];
+var newArray=friends.slice(0,3);
+var aa=friends.slice();
+
+
+//---
+undefined
+var friends=["Charlie","Liz","Peter","Liz"];
+
+undefined
+var newArray=friends.slice(0,3);
+
+undefined
+newArray
+(3) ["Charlie", "Liz", "Peter"]
+var newArray=friends.slice(0,2);
+
+undefined
+newArray
+(2) ["Charlie", "Liz"]
+var aa=friends.slice();
+undefined
+aa
+(4) ["Charlie", "Liz", "Peter", "Liz"]
