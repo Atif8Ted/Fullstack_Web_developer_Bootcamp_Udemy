3b1bf287baa263248e7318d1068d954ade34930a
Atif8Ted <mohd.atifimam@hotmail.com>
2017-09-12 01:53:23 +0530

    added

--- javascript/array_method.js
+++ javascript/array_method.js
@@ -1,4 +1,15 @@
-//push
+//push and pop 
 var colours=["red","yellow"];
 colours.push("indigo");
 colours.pop()
+
+//shift and unshift --> add or remove in beginning of array
+
+colours.unshift("infrared");
+colours.shift();
+
+//indexof
+var friends=["Charlie","Liz","Peter","Liz"];
+friends.indexOf("Liz");
+friends.indexOf("Liz");
+friends.indexOf("Swati");
